7a8015772277b711d240943af2012ccc9820b99c
TheAstartes <46090816+TheAstartes@users.noreply.github.com>
2020-03-04 13:09:51 +0100

    Update self_driving_car.py

--- self_driving_car.py
+++ self_driving_car.py
@@ -130,7 +130,7 @@ print(x_train.shape)
 
 def nvidia_model():
   model = Sequential()
-  model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='relu'))
+  model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(66, 200, 3), activation='elu'))
   model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='elu'))
   model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='elu'))
   model.add(Convolution2D(64, 3, 3, activation='elu'))
@@ -168,5 +168,4 @@ plt.xlabel('Epoch')
 
 model.save('model.h5')
 
-from google.colab import files
-files.download('model.h5')
+
